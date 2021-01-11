6373f70334dd6275d45b7d2b9238f137c441ac64
tounsiaziz <aziztounsi99@gmail.com>
2021-01-11 23:50:34 +0100

    88

--- mainM.js
+++ mainM.js
@@ -1,5 +1,27 @@
+
+
+
 $(document).ready(function(){
 		$("#btn").on('click',function(){
-			$("#specs1").toggle();
+			$("#specs1").toggle(2000);
 		});
 	});
+
+
+$(document).ready(function(){
+		$("#bbs").on('click',function(){
+			$("#specs3").toggle(2000);
+		});
+	});
+
+$(document).ready(function(){
+		$("#but").on('click',function(){
+			$("#specs4").toggle(2000);
+		});
+	});
+
+
+
+
+
+
